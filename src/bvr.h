5117faff6757d97a0c3e7ec42cc880174197fb89
Anton L. Šijanec <anton@sijanec.eu>
2020-05-05 19:33:53 +0200

    forgot to increment version number, lol

--- src/bvr.h
+++ src/bvr.h
@@ -23,7 +23,7 @@ char bvr_variables[BVR_INITIAL_VARIABLES_COUNT*2][BVR_MAX_VARIABLE_SIZE];
 int bvr_bvrvar_first_time_set = 1;
 
 #define BVR_VER_MAJOR 0
-#define BVR_VER_MINOR 0
+#define BVR_VER_MINOR 1
 #define BVR_VER_PATCH 0
 
 #define BVR_PATH_SEPARATOR ';'
